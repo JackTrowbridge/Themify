9d24466c3165a37c4127c663f228f6df2d4979ac
Jack .T <jack@reego.org>
2021-12-25 16:24:24 +0000

    initial commit

---
+++ src/dev/jayjaytee/themify/Main.java
@@ -0,0 +1,10 @@
+package dev.jayjaytee.themify;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Created by Jack Trowbridge");
+        System.out.println("Starting Themify...");
+    }
+
+}
